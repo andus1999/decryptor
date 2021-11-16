3a70bba4f42627ea6191ee43115246535c89ffb8
andi <sepphirnoederl@gmail.com>
2021-11-16 05:04:05 +0100

    added analytics event to purchase success

--- src/pages/Api.js
+++ src/pages/Api.js
@@ -5,11 +5,53 @@ import Footer from '../components/Footer'
 import InfoSection from '../components/InfoSection'
 import { apiDescription } from '../components/InfoSection/Data'
 import LogoBanner from '../components/LogoBanner'
+import {getAnalytics, logEvent} from 'firebase/analytics'
+import {useStripe} from '@stripe/react-stripe-js';
 
 const Api = (props) => {
-    React.useEffect(() => {
-        window.scrollTo(0,0)        
-    }, [])
+
+  const stripe = useStripe();
+  const analytics = getAnalytics();
+
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    window.scrollTo(0,0)        
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          logEvent(analytics, 'purchase', {
+              value: paymentIntent.metadata.amount/100,
+              currency: paymentIntent.metadata.currency,
+              uid: paymentIntent.metadata.uid,
+          })  
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
     return (
         <div style={{backgroundColor: 'white'}}>
             <LogoBanner user={props.user}/>
