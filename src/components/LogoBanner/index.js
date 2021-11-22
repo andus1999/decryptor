fd946cec00c0032002122e93973eb17b511fa6e4
andi <sepphirnoederl@gmail.com>
2021-11-22 17:11:48 +0100

    added arrow navigation

--- src/components/LogoBanner/index.js
+++ src/components/LogoBanner/index.js
@@ -3,13 +3,32 @@ import { NavbarContainer, NavLogo, Nav, NavBtn } from '../Navbar/NavbarElements'
 import { getAuth, signOut } from "firebase/auth";
 import {Link} from 'react-router-dom'
 import Button from '@mui/material/Button'
+import {MdKeyboardArrowLeft} from 'react-icons/md'
+import { withRouter, useHistory } from 'react-router';
+
 
 const LogoBanner = (props) => {
+    const history = useHistory();
     return (
         <>
             <Nav scrollNav={true}>
                 <NavbarContainer>
-                    <NavLogo to='/'>decryptor</NavLogo>
+                    <div style={{
+                        display: 'flex',
+                    }}>
+                        <NavBtn>
+                            <Button 
+                                color='white'
+                                onClick={() => history.goBack()}>
+                                <MdKeyboardArrowLeft style={{
+                                    fontSize: '30px',
+                                }}/>
+                            </Button>
+                        </NavBtn>
+                        <NavLogo to='/'>
+                            decryptor
+                        </NavLogo>
+                    </div>
                     <NavBtn>
                             {props.user ? (
                                 <Button 
