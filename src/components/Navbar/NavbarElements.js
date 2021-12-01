54d3e261f192728da1b5fcb88619e0d3e3300a5b
andi <sepphirnoederl@gmail.com>
2021-12-01 05:27:56 +0100

    position sticky changed to fixed

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -6,12 +6,12 @@ import Colors from '../../styles/Colors';
 export const Nav = styled.nav`
     background: ${({ scrollNav }) => (scrollNav ? Colors.black : 'transparent')};
     height: 80px;
-    margin-top: -80px;
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: 1rem;
-    position: sticky;
+    position: fixed;
     top: 0;
     z-index: 10;
 `;
