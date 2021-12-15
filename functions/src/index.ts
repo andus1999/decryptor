497ac3af12a5033e084e2777ba7c18fc572f97ef
andi <sepphirnoederl@gmail.com>
2021-12-15 01:42:04 +0100

    added free api calls refresh schedule

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,6 +13,7 @@ import api1 from "./api1";
 import stripe from "./stripe";
 
 const db = getFirestore();
+const dailyFreeApiCalls = 10000;
 
 exports.api1 = api1;
 exports.stripe = stripe;
@@ -23,7 +24,7 @@ exports.createUser = functions.auth.user().onCreate(async (user) => {
     name: user.displayName,
     email: user.email,
     uid: user.uid,
-    freeApiCalls: 10000,
+    freeApiCalls: dailyFreeApiCalls,
     paidApiCalls: 0,
   };
   await docRef.set(data);
@@ -34,4 +35,11 @@ exports.cleanupUser = functions.auth.user().onDelete(async (user) => {
   const docRef = db.collection("users").doc(user.uid);
   await docRef.delete();
   return;
+});
+
+exports.refreshFreeApiCalls = functions.pubsub.schedule("every day 00:00").onRun(async () => {
+  const batch = db.batch();
+  const query = await db.collection("users").where("freeApiCalls", "<", dailyFreeApiCalls).get();
+  query.forEach((d) => batch.update(d.ref, {freeApiCalls: dailyFreeApiCalls}));
+  await batch.commit();
 });
