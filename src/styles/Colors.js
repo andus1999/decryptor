691a7e9a909fcb19ba5ac84f250482d085db9620
andi <sepphirnoederl@gmail.com>
2021-12-16 13:22:10 +0100

    🎨

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -5,12 +5,12 @@ const main = pink;
 const Colors = {
   white: '#ffffff',
   black: '#121212',
-  grey: '#888888',
-  lightGrey: '#dddddd',
+  grey: grey[600],
+  lightGrey: grey[400],
   primary: main[500],
   primaryLight: main[300],
   colorOnPrimary: '#ffffff',
-  shadow: '#f500570a',
+  shadow: main[500] + '0f',
 };
 
 export default Colors;
