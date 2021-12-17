f11857e0746c9179911aa4a257726e087acff82b
andi <sepphirnoederl@gmail.com>
2021-12-17 14:05:36 +0100

    added noreply email notifications

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from "firebase-functions";
 import admin = require("firebase-admin");
+import {config} from "dotenv";
+config();
 
 const serviceAccount = require("../decryptor-admin.json");
 
@@ -11,9 +13,26 @@ admin.initializeApp({
 import {getFirestore} from "firebase-admin/firestore";
 import api1 from "./api1";
 import stripe from "./stripe";
+import {SMTPClient, Message} from "emailjs";
 
 const db = getFirestore();
 const dailyFreeApiCalls = 10000;
+const mailClient = new SMTPClient({
+  user: "noreply@decryptor.xyz",
+  password: process.env.NOREPLY_PW,
+  host: "smtppro.zoho.eu",
+  tls: true,
+});
+
+const sendEmail = async (to: string, subject: string, body: string) => {
+  const message = new Message({
+    text: body,
+    from: "noreply@decryptor.xyz",
+    to,
+    subject,
+  });
+  await mailClient.sendAsync(message);
+};
 
 exports.api1 = api1;
 exports.stripe = stripe;
@@ -28,6 +47,15 @@ exports.createUser = functions.auth.user().onCreate(async (user) => {
     paidApiCalls: 0,
   };
   await docRef.set(data);
+  if (user.email != undefined) {
+    sendEmail(
+        user.email,
+        "Welcome to decryptor",
+        (user.displayName ? `Hello ${user.displayName}!` : "Hello!") +
+        "\n\nWelcome to decryptor.\nThanks for signing up!" +
+        "\n\nThanks,\nThe decryptor team",
+    );
+  }
   return;
 });
 
@@ -36,6 +64,24 @@ exports.cleanupUser = functions.auth.user().onDelete(async (user) => {
   await docRef.delete();
   return;
 });
+
+exports.issueNotification = functions.firestore
+    .document("aggregations/issues").onUpdate((change) => {
+      Object.keys(change.after.data()).forEach((it) => {
+        const newData = change.after.data()[it];
+        const before = change.before.data()[it]?.length || 0;
+        const after = newData.length;
+        if (after > before) {
+          sendEmail(
+              "support@decryptor.xyz",
+              "New issue",
+              "A new issue has been added to the issues list:\n" +
+              `${it}: ${JSON.stringify(newData[after - 1])}` +
+              "\n\nPlease review it as soon as possible.",
+          );
+        }
+      });
+    });
 
 exports.refreshFreeApiCalls = functions.pubsub.schedule("0 0 * * *")
     .timeZone("Etc/UTC")
